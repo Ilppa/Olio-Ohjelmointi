729b2aa21d21c5b309d90ebe4c92605349665268
Ilppa <imkyrolainen@gmail.com>
2025-02-02 22:09:12 +0200

    Update Asiakas.cpp

--- Olio-Ohjelmointi/Tehtävät/VKO4/Asiakas.cpp
+++ Olio-Ohjelmointi/Tehtävät/VKO4/Asiakas.cpp
@@ -1,27 +1,34 @@
 #include "Asiakas.h"
 
-Asiakas::Asiakas(std::string n, double luottoraja) : nimi(n), luottotili(luottoraja) {}
+Asiakas::Asiakas(std::string n, double pankkiSaldo)
+    : nimi(n), pankkitili(pankkiSaldo), luottotili() {  // Luottotilin saldo aluksi nolla
+}
 
 void Asiakas::showSaldo() const {
     std::cout << nimi << " - Pankkitili: " << pankkitili.getBalance()
-              << ", Luottotili: " << luottotili.getBalance() << "\n";
+    << ", Luottotili: " << luottotili.getBalance() << " (Luottoraja: "
+    << luottotili.getLuottoraja() << ")\n";
 }
 
-bool Asiakas::talletus(double amount) {
-    return pankkitili.deposit(amount);
-}
 
-bool Asiakas::nosto(double amount) {
-    return pankkitili.withdraw(amount);
-}
+bool Asiakas::talletus(double amount) { return pankkitili.deposit(amount); }
+bool Asiakas::nosto(double amount) { return pankkitili.withdraw(amount); }
 
 bool Asiakas::luotonNosto(double amount) {
-    return luottotili.withdraw(amount);
+    if (luottotili.withdraw(amount)) {
+        pankkitili.deposit(amount);  // Siirretään nostettu summa pankkitilille
+        return true;
+    }
+    return false;
 }
 
 bool Asiakas::luotonMaksu(double amount) {
-    return luottotili.deposit(amount);
+    if (amount > 0 && pankkitili.withdraw(amount)) {  // Vähennetään summa pankkitililtä
+        return luottotili.deposit(amount);  // Maksetaan summa luottotilille
+    }
+    return false;
 }
+
 
 bool Asiakas::tiliSiirto(double amount, Asiakas &vastaanottaja) {
     if (amount > 0 && pankkitili.withdraw(amount)) {
@@ -29,3 +36,20 @@ bool Asiakas::tiliSiirto(double amount, Asiakas &vastaanottaja) {
     }
     return false;
 }
+
+std::string Asiakas::getNimi() const {
+    return nimi;
+}
+
+Pankkitili& Asiakas::getPankkitili() {
+    return pankkitili;
+}
+
+Luottotili& Asiakas::getLuottotili() {
+    return luottotili;
+}
+
+void Asiakas::nollaaLuotto() {
+    luottotili.deposit(luottotili.getBalance());  // Nollaa luottotilin saldon
+}
+
