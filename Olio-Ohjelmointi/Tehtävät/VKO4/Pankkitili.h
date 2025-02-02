4dcdb53eceb384c1fd45c4e15575f18da13e53b9
Ilppa <imkyrolainen@gmail.com>
2025-02-02 22:11:22 +0200

    Update Pankkitili.h

--- Olio-Ohjelmointi/Tehtävät/VKO4/Pankkitili.h
+++ Olio-Ohjelmointi/Tehtävät/VKO4/Pankkitili.h
@@ -9,11 +9,11 @@ protected:
     double saldo;
 
 public:
-    Pankkitili();
+    Pankkitili(double initialSaldo = 0);
     virtual ~Pankkitili();
-    virtual bool deposit(double amount);
-    virtual bool withdraw(double amount);
-    double getBalance() const;
+    virtual bool deposit(double amount);  // Talletus
+    virtual bool withdraw(double amount); // Nosto
+    double getBalance() const;            // Pankkitilin saldo
 };
 
 #endif // PANKKITILI_H
