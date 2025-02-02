a3bb48dd8bcce51aed2be6c36ff4a1ce7f1967e7
Ilppa <imkyrolainen@gmail.com>
2025-02-02 20:34:30 +0200

    Luottotili.h

---
+++ Olio-Ohjelmointi/Tehtävät/VKO4/Luottotili.h
@@ -0,0 +1,16 @@
+#ifndef LUOTTOTILI_H
+#define LUOTTOTILI_H
+
+#include "Pankkitili.h"
+
+class Luottotili : public Pankkitili {
+private:
+    double luottoraja;
+
+public:
+    Luottotili(double raja);
+    bool withdraw(double amount) override;
+    bool deposit(double amount) override;
+};
+
+#endif // LUOTTOTILI_H
